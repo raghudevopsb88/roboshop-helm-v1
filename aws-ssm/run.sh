9ad64f785a9da2a8b963473b26242fd3a08b5406
Raghu K <raghuk.vit@gmail.com>
2026-05-28 07:53:40 +0530

    RoboShop scripts

--- aws-ssm/run.sh
+++ aws-ssm/run.sh
@@ -1,6 +1,11 @@
 [ -z "${SERVICE_NAME}" ] && exit
 [ -z "${PARAMS}" ] && exit
 
+echo PARAMS - ${PARAMS}
+echo SERVICE_NAME - $SERVICE_NAME
+
+set -x
+
 for i in $PARAMS ; do
  aws ssm get-parameters --names "/${SERVICE_NAME}/$i" --region us-east-1 --with-decryption | jq -r '.Parameters[] | "\(.Name)=\"\(.Value)\""' | sed -e "s|/${SERVICE_NAME}/|export |" | tee -a /data/params
 done
