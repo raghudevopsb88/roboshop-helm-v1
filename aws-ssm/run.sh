c2a142a2db8da5d997c0d418f19b4b7a0d92e8c2
Raghu K <raghuk.vit@gmail.com>
2026-05-28 08:22:54 +0530

    scripts: update 1 file(s) (run.sh)

--- aws-ssm/run.sh
+++ aws-ssm/run.sh
@@ -8,4 +8,4 @@ for i in $PARAMS ; do
  aws ssm get-parameters --names "/${SERVICE_NAME}/$i" --region us-east-1 --with-decryption | jq -r '.Parameters[] | "\(.Name)=\"\(.Value)\""' | sed -e "s|/${SERVICE_NAME}/|export |" | tee -a /data/params
 done
 
-#
+cat /data/params
